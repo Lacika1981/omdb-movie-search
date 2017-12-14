2139ccad2b8dd681cbf697f4248deeffa6c38316
Laszlo Varga <vargalaszlo1981@googlemail.com>
2017-12-14 21:43:20 +0000

    working on the layout, starting new method to change imdbRating's background color dinamically

--- src/Information.js
+++ src/Information.js
@@ -11,34 +11,51 @@ const Information = (props) => {
                   </div>
             )
       }
+
+      this.handleColor = () => {
+            return 'red';
+      }
+
+      const styles = {
+            width: ((props.value.data.imdbRating) * 10) +"%",
+            background: this.handleColor()
+      }
+
       return (
-            <div className="container">
-            <div className="row">
-            <div className="col-md-9">
-            <div  className="text-center movieTitle">
-                  <h4>Movie title:</h4>
-                  <p className="pInline">{props.value.data.Title} <p className="pInline">({props.value.data.Year})</p></p>
-                  </div>
-                  <h4>Actors:</h4>
-                  <p>{props.value.data.Actors}</p>
-                  <h4>Director:</h4>
-                  <p>{props.value.data.Director}</p>
-                  <h4>Metascore:</h4>
-                  <p>{props.value.data.Metascore}</p>
-                  <h4>Production:</h4>
-                  <p>{props.value.data.Production}</p>
-                  <h4>Released:</h4>
-                  <p>{props.value.data.Released}</p>
-                  <h4>imdbRating:</h4>
-                  <p>{props.value.data.imdbRating}</p>
-                  <h4>Plot:</h4>
-                  <p>{props.value.data.Plot}</p>
-                  <h4>Awards:</h4>
-                  <p>{props.value.data.Awards}</p>
-                  </div>
-                  <div className="col-md-3">
-                  <img src={props.value.data.Poster} alt='poster'/>
-                  </div>
+            <div className="container information">
+                  <div className="row">
+                        <div className="col-md-9">
+                              <div className="text-center movieTitle">
+                                    <h4>Movie title:</h4>
+                                    <p className="pInline">{props.value.data.Title}</p>
+                                    <p className="pInline">({props.value.data.Year})</p>
+                              </div>
+                              <div className="row">
+                                    <div className="col-md-6 information-col-one">
+                                          <h4>Actors:</h4>
+                                          <p>{props.value.data.Actors}</p>
+                                          <h4>Director:</h4>
+                                          <p>{props.value.data.Director}</p>
+                                          <h4>Metascore:</h4>
+                                          <p>{props.value.data.Metascore}</p>
+                                          <h4>Production:</h4>
+                                          <p>{props.value.data.Production}</p>
+                                          <h4>Released:</h4>
+                                          <p>{props.value.data.Released}</p>
+                                          <h4>imdbRating:</h4>
+                                          <p className="progress-bar" role="progressbar" style={styles} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">{props.value.data.imdbRating}</p>
+                                    </div>
+                                    <div className="col-md-6 information-col-two">
+                                          <h4>Plot:</h4>
+                                          <p className="text-justify">{props.value.data.Plot}</p>
+                                          <h4>Awards:</h4>
+                                          <p>{props.value.data.Awards}</p>
+                                    </div>
+                              </div>
+                        </div>
+                        <div className="col-md-3">
+                              <img src={props.value.data.Poster} className="img-fluid" alt='poster'/>
+                        </div>
                   </div>
             </div>
       )
